6ee69e73448dd600f5bb381b4345f8c750e0925a
Randall <rpx@randalls-air.attlocal.net>
2021-02-15 15:47:58 -0500

    fix prod config for photomarket

--- config/prod.exs
+++ config/prod.exs
@@ -5,6 +5,6 @@ config :nostrum,
   num_shards: :auto
 
 config :edison,
-  mechmarket_role_id: System.get_env("EDISON_MECHMARKET_ROLE_ID") |> String.to_integer(),
-  mechmarket_channel: System.get_env("EDISON_MECHMARKET_CHANNEL") |> String.to_integer(),
-  mechmarket_query: System.get_env("EDISON_MECHMARKET_QUERY")
+  photomarket_role_id: System.get_env("EDISON_PHOTOMARKET_ROLE_ID") |> String.to_integer(),
+  photomarket_channel: System.get_env("EDISON_PHOTOMARKET_CHANNEL") |> String.to_integer(),
+  photomarket_query: System.get_env("EDISON_PHOTOMARKET_QUERY")
